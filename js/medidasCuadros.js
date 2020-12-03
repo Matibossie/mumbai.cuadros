1dcce32c8d82a40a134f3479670513b4b23f2520
Matias Bossie <mati.bossie@hotmail.com>
2020-12-02 21:48:04 -0300

    todo ok

---
+++ js/medidasCuadros.js
@@ -0,0 +1,16 @@
+class dimension {
+    constructor(id, medida, rotacion, precio) {
+        this.id = id;
+        this.medida = medida;
+        this.rotacion = rotacion;
+        this.precio = precio;
+    }
+}
+let dimension1 = new dimension("20x30V","20x30","vertical",500);
+let dimension2 = new dimension("30x40V","30x40","vertical",700);
+let dimension3 = new dimension("32x47V","32x47","vertical",900);
+let dimension4 = new dimension("20x30H","20x30","horizontal",500);
+let dimension5 = new dimension("30x40H","30x40","horizontal",700);
+let dimension6 = new dimension("32x47H","32x47","horizontal",900);
+
+
--- js/tiendaOperacional.js
+++ js/tiendaOperacional.js
@@ -6,29 +6,30 @@
 
 var inicio = 0;
 
+
+
 function incrementar(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
     if(inicio==10){
         alert("Limite alcanzado");
     }else{
-    var cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del cantidad, y se incrementa en 1 el valor que tenga.
+    cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del cantidad, y se incrementa en 1 el valor que tenga.
     //console.log(inicio);
     }
-}
+};
 function decrementar(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
     if(inicio==0){
         alert("Elija más que 0 cuadros");
     }else{
-    var cantidad = document.getElementById('cantidad').value = --inicio; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    cantidad = document.getElementById('cantidad').value = --inicio; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
     //console.log(inicio);
     }
-}
-
+};
 document.getElementById("next").onclick = function() {
     if(inicio==0){
         alert("Elija más que 0 cuadros");
     }else{
         sessionStorage.setItem('cantidad',inicio);
-        console.log(inicio);
+        //console.log(inicio);
         document.getElementById("PasoUno").classList.add("hide");
         document.getElementById("PasoDos").classList.remove("hide");
     }
@@ -37,136 +38,137 @@ document.getElementById("previous").onclick = function() {
     document.getElementById("PasoUno").classList.remove("hide");
     document.getElementById("PasoDos").classList.add("hide");
 }
-
 document.getElementById("Horizontal").onclick = function() {
     document.getElementById("medidasVerticales").classList.add("hide");
-    document.getElementById("medidasHorizontales").classList.remove("hide");    
+    document.getElementById("medidasHorizontales").classList.remove("hide");
 }
-
 document.getElementById("Vertical").onclick = function() {
     document.getElementById("medidasHorizontales").classList.add("hide");
     document.getElementById("medidasVerticales").classList.remove("hide");
+};
+ 
+var sumaCuadros = parseInt(document.getElementById('valor1').value)+parseInt(document.getElementById('valor2').value)+parseInt(document.getElementById('valor3').value)+parseInt(document.getElementById('valor4').value)+parseInt(document.getElementById('valor5').value)+parseInt(document.getElementById('valor6').value);
+
+
+var value1 = 0;
+
+function positivo20x30V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value1==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor1 = document.getElementById('valor1').value = ++value1; //se obtiene el valor del valor1, y se incrementa en 1 el valor que tenga.
+    //console.log(value1);
+    }
+}
+function negativo20x30V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value1==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor1 = document.getElementById('valor1').value = --value1; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value1);
+    }
 }
 
+var value2 = 0;
+
+function positivo30x40V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value2==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor2 = document.getElementById('valor2').value = ++value2; //se obtiene el valor del valor1, y se incrementa en 1 el valor que tenga.
+    //console.log(value2);
+    }
+}
+function negativo30x40V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value2==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor2 = document.getElementById('valor2').value = --value2; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value2);
+    }
+}
+
+var value3 = 0;
+
+function positivo32x47V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value3==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor3 = document.getElementById('valor3').value = ++value3; //se obtiene el valor del valor1, y se incrementa en 1 el valor que tenga.
+    //console.log(value3);
+    }
+}
+function negativo32x47V(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value3==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor3 = document.getElementById('valor3').value = --value3; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value3);
+    }
+}
+
+var value4 = 0;
+
+function positivo20x30H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value4==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor4 = document.getElementById('valor4').value = ++value4; //se obtiene el valor del valor4, y se incrementa en 1 el valor que tenga.
+    //console.log(value4);
+    }
+}
+function negativo20x30H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value4==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor4 = document.getElementById('valor4').value = --value4; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value4);
+    }
+}
+
+var value5 = 0;
+
+function positivo30x40H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value5==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor5 = document.getElementById('valor5').value = ++value5; //se obtiene el valor del valor1, y se incrementa en 1 el valor que tenga.
+    //console.log(value5);
+    }
+}
+function negativo30x40H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value5==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor5 = document.getElementById('valor5').value = --value5; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value5);
+    }
+}
+
+var value6 = 0;
+
+function positivo32x47H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
+    if(value6==10){
+        alert("Limite alcanzado");
+    }else{
+    var valor6 = document.getElementById('valor6').value = ++value6; //se obtiene el valor del valor1, y se incrementa en 1 el valor que tenga.
+    //console.log(value6);
+    }
+}
+function negativo32x47H(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id disminuir
+    if(value6==0){
+        alert("Elija más que 0 cuadros");
+    }else{
+    var valor6 = document.getElementById('valor6').value = --value6; //se obtiene el valor del input, y se decrementa en 1 el valor que tenga.
+    //console.log(value6);
+    }
+}
+
+//Declarar una variable con toda la sumatoria de los value
+//
+var sumanding = (value1 + value2 + value3 + value4 +value5 + value6);
 
 
+document.getElementById("next2").onclick = function() {
 
-
-
-
-
-//Boton Continuar
-
-// var next = document.createElement('button'); next.setAttribute("class", "next boton"); next.setAttribute("id", "next");
-// var siguiente = document.createTextNode("Continuar");
-// next.appendChild(siguiente);
-// document.getElementById("contenedor").appendChild(next);
-
-
-// // Paso 2
-// //Titulo
-
-// var segundo = document.createTextNode("2"); numeroDePasos.appendChild(numeroUno); numeroUno.appendChild(primero);
-// var segundotexto = document.createTextNode("¡Elegí el tamaño y orientación de tus cuadros!"); tituloDePaso.appendChild(textoPaso); textoPaso.appendChild(texto);
-
- 
- 
-// var contenedorMedidas = document.createElement('div'); contenedorMedidas.setAttribute("class", "contenedorMedidas"); contenedorMedidas.setAttribute("id", "contenedorMedidas");
-
-
-
-// function cambiarNumeroDePaso(){
-//     document.getElementById("numeroUno").innerHTML = "2";
-// }
-// function cambiarTituloDePaso(){
-//     document.getElementById("textoPaso").innerHTML = "¡Elegí las medidas y orientación de tus cuadros!"; 
-// }
-// document.getElementById("next").onclick = function() {
-//     if(inicio==0){
-//         alert("Elija más que 0 cuadros");
-//     }else{
-//         sessionStorage.setItem('cantidad',inicio);
-//         // console.log(inicio);
-//         document.getElementById("contenedor").removeChild(contador); 
-//         document.getElementById("contenedor").removeChild(next); 
-//         cambiarNumeroDePaso();
-//         cambiarTituloDePaso();   
-
-//         document.getElementById("contenedor").appendChild(contenedorMedidas);
-
-//         c2030h.appendChild(c20x30h);
-//         document.getElementById("contenedorMedidas").appendChild(c2030h);
-
-//         c2030v.appendChild(c20x30v);
-//         document.getElementById("contenedorMedidas").appendChild(c2030v);
-
-//         c3030h.appendChild(c30x30h);
-//         document.getElementById("contenedorMedidas").appendChild(c3030h);
-
-//         c3030v.appendChild(c30x30v);
-//         document.getElementById("contenedorMedidas").appendChild(c3030v);
-
-//         c3040h.appendChild(c30x40h);
-//         document.getElementById("contenedorMedidas").appendChild(c3040h);
-
-//         c3040v.appendChild(c30x40v);
-//         document.getElementById("contenedorMedidas").appendChild(c3040v);
-
-//         c3247h.appendChild(c32x47h);
-//         document.getElementById("contenedorMedidas").appendChild(c3247h);
-
-//         c3247v.appendChild(c32x47v);
-//         document.getElementById("contenedorMedidas").appendChild(c3247v);
-
-//         c4060h.appendChild(c40x60h);
-//         document.getElementById("contenedorMedidas").appendChild(c4060h);
-
-//         c4060v.appendChild(c40x60v);
-//         document.getElementById("contenedorMedidas").appendChild(c4060v);
-
-//         c5070h.appendChild(c50x70h);
-//         document.getElementById("contenedorMedidas").appendChild(c5070h);
-
-//         c5070v.appendChild(c50x70v);
-//         document.getElementById("contenedorMedidas").appendChild(c5070v);
-        
-//         document.getElementById("contenedor").appendChild(next);
-
-//     }
-// }
-
-
-
-
- 
-
-
-// function registrar() {
-//   // console.log(" funciona");
-//   function Medidas(anchoalto,horientacion){
-//     this.anchoalto = anchoalto;
-//     this.horientacion = horientacion;
-//   }
-//   let registrarAnchoalto = document.getElementById("anchoalto").value;
-//   // console.log(registrarAnchoalto);
-//     let registrarHorientacion = document.getElementById("horientacion").value;
-//   // console.log(registrarHorientacion);
-
-//   nuevaMedida = new Medidas(registrarAnchoalto,registrarHorientacion);
-//       //console.log(nuevaMedida);
-//       agregar();
-//     } 
-
-//   let baseMedidas = [];
-//   function agregar() {
-
-//       for (baseMedidas == 0; baseMedidas.length < enviarCantidad; (baseMedidas.push(nuevaMedida))++) {
-//         document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevaMedida.anchoalto+'</td><td>'+nuevaMedida.horientacion+'</td></tbody>';
-//       }  
-
-//     };
- 
-
-
-
-
+}
